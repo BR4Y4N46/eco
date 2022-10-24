480ad20582307e749aeb7fbdb9c2e433d3c4aa37
Brayan <herrebra7@gmail.com>
2022-10-24 12:08:51 -0500

    modifique

--- controllers/crud.js
+++ controllers/crud.js
@@ -41,7 +41,7 @@ exports.save = (req, res)=>{
        if(error) {
           console.log(error);
     } else{
-      res.redirect('./');
+      res.redirect('/');
       console.log('_________________Sucessfull_____________________'.green);
     } 
 })
