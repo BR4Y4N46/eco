596e7cde5b0735a221e35a78afa46d0dc4e32626
Brayan <herrebra7@gmail.com>
2022-10-24 12:04:44 -0500

    modifique

--- router.js
+++ router.js
@@ -50,7 +50,7 @@ const conexion = require('./database/db');
 
 //});
 const crud = require('./controllers/crud');
-router.post('/index.html', crud.save);
+router.post('/save', crud.save);
 //router.post('/update', crud.update);
 
 module.exports = router
